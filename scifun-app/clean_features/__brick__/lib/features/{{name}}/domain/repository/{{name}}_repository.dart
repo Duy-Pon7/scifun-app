f5ad9c973985a877da32516c46e01e22a4dba7dd
Truong Quoc Duy <duytrquoc@gmail.com>
2026-06-05 08:51:08 +0700

    .

--- scifun-app/clean_features/__brick__/lib/features/{{name}}/domain/repository/{{name}}_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:sci_fun/core/error/failure.dart';
-import 'package:sci_fun/features/subject/domain/entity/subject_entity.dart';
-
-abstract interface class SubjectRepository {
-  Future<Either<Failure, List<SubjectEntity>>> getAllSubjects(
-      String? searchQuery);
-}
